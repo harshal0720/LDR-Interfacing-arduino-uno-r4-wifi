3fa5ca8adc1017e0449b3f5ca2d90d5537d5ed69
harshal0720 <harshalgautam2008@gmail.com>
2026-01-30 10:09:31 +0530

    add LDR sensor reading and serial output to main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <Arduino.h>
+#define LDR_PIN A0
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    while (!Serial); //import for uno r4 wifi
 }
 
 void loop() {
-// write your code here
+    int ldrRaw = analogRead(LDR_PIN);
+    //map to percentage
+    int lightPercentage = map(ldrRaw, 0, 1023, 0, 100);
+    Serial.print("LDR Raw Value: ");
+    Serial.print(ldrRaw);
+    Serial.print(" | Light Intensity: ");
+    Serial.print(lightPercentage);
+    Serial.println("%");
+    delay(500);
 }
